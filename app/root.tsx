f082e646780186981cd3c2114f6143365549b428
iiishi9779 <ezimadzez@gmail.com>
2024-03-05 23:25:24 +0500

    shadcn setup

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,7 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
-import styles from "~/styles/tailwind.css?url";
+import styles from "~/styles/globals.css?url";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: styles },
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,16 @@
 import type { MetaFunction } from "@remix-run/node";
+import { PlusIcon } from "@radix-ui/react-icons"
+import { Input } from '~/components/ui/input'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card"
+import { Button } from "~/components/ui/button";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "~/components/ui/dialog";
+import { Outlet } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,8 +21,32 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <h1 className="text-3xl font-bold text-blue-600">
-      Obese Bird Template
-    </h1>
+    <div className="h-screen flex justify-center items-center bg-slate-100">
+      <Card className="w-[380px]">
+        <CardHeader className="flex-row justify-between items-center">
+          <CardTitle>Users</CardTitle>
+
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="outline">
+                <PlusIcon />
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <Outlet />
+              <DialogHeader>
+                <DialogTitle>Add User</DialogTitle>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+
+        </CardHeader>
+        <CardContent>
+          <Input placeholder="search for username" className="bg-slate-100" />
+
+        </CardContent>
+      </Card>
+    </div>
+
   );
 }
